5acf8087970dbb40579d114f2ae6c1167f97bc51
Tymm Schmitke <ifellinaholeonce@gmail.com>
2017-12-04 15:58:00 +0000

    passing in Options to request function

--- printHTML.js
+++ printHTML.js
@@ -1,12 +1,32 @@
-var getAndPrintHTML = function () {
-   var https = require('https');
+// var getAndPrintHTML = function () {
+//    var https = require('https');
 
-  var requestOptions = {
-    host: 'sytranis.github.io',
-    path: '/http-examples/step1.html'
-  };
+//   var requestOptions = {
+//     host: 'sytranis.github.io',
+//     path: '/http-examples/step1.html'
+//   };
 
-  https.get(requestOptions, function (res) {
+//   https.get(requestOptions, function (res) {
+//     res.setEncoding('utf8');
+
+//     let accumulatedData = '';
+//     res.on('data', function (data) {
+//       accumulatedData += data;
+//     });
+
+//     res.on('end', function () {
+//       console.log(accumulatedData);
+//     });
+//   });
+// };
+
+// getAndPrintHTML();
+
+
+var getAndPrintHTML = function (options) {
+  var https = require('https');
+
+  https.get(options, function (res) {
     res.setEncoding('utf8');
 
     let accumulatedData = '';
@@ -20,4 +40,11 @@ var getAndPrintHTML = function () {
   });
 };
 
-getAndPrintHTML();
+
+
+var requestOptions = {
+  host: 'sytranis.github.io',
+  path: '/http-examples/step1.html'
+};
+
+getAndPrintHTML(requestOptions);
